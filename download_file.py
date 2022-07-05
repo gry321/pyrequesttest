19d12d50315043b19159005b72ae0ebadd9ae3e9
gry321 <yao1127@aliyun.com>
2022-07-05 17:36:09 +0800

    PyRequestTest Update!!!

--- download_file.py
+++ download_file.py
@@ -3,6 +3,7 @@ import os
 import sys
 import urllib.parse as up
 import re
+import traceback
 
 
 class UrlIsError(Exception):
@@ -17,8 +18,12 @@ else:
 try:
     filename = sys.argv[2]
 except Exception:
-    filename = up.urlsplit(
+    g = up.urlsplit(
         url).path.split("/")[-1]
+    if g == "":
+        filename = up.urlsplit(url).hostname.split(".")[1]
+    else:
+        filename = g
 try:
     speed = sys.argv[3]
     speed = int(speed)
@@ -46,7 +51,7 @@ def main():
         ok = True
         is_open = False
         if filename in os.listdir():
-            p = input("那个目录下有这个文件，请指示\nw: 覆盖\nq: 结束")
+            p = input("那个目录下有这个文件：%s，请指示\nw: 覆盖\nq: 结束\n" % filename)
             if p.upper() == "W":
                 f = open(filename, "wb+")
                 is_open = True
@@ -56,14 +61,37 @@ def main():
             return None
         if is_open == False:
             f = open(filename, "wb+")
-        for x in html.iter_content(chunk_size=speed):
-            f.write(x)
-            echo_num += len(x)
-            # print("一共写入%s个字符" % str(echo_num))
-            print("本次写入{}个字符，一共写入{}个字符".format(str(len(x)), str(echo_num)))
-            # print("本次写入%s个字符" % str(len(x)))
-        f.close()
-        print("下载完成")
+
+        is_say = False
+        is_close = False
+
+        content = html.content
+        num = len(content)
+        contents = html.iter_content(chunk_size=speed)
+        print("本次下载字节数："+str(num))
+        for x in contents:
+            try:
+                f.write(x)
+                echo_num += len(x)
+                # print("一共写入%s个字符" % str(echo_num))
+                # print("本次写入{}个字符，一共写入{}个字符".format(str(len(x)), str(echo_num)))
+                # print("本次写入{}个字符，一共写入{}个字符，还差{}个字符结束".format(str(len(x)), str(echo_num), str(num-echo_num)))
+                print("本次写入{}个字符，一共写入{}个字符，还差{}个字符结束， 下载进度：{:.2f}%".format(
+                    str(len(x)), str(echo_num), str(num-echo_num), float(echo_num)/float(num)*100))
+                # print("本次写入%s个字符" % str(len(x)))
+            except Exception:
+                traceback.print_exc()
+                is_say = True
+                is_close = True
+                print("下载失败")
+                f.close()
+                break
+        if is_say == False:
+            f.close()
+            is_say = True
+        if is_close == False:
+            print("下载完成")
+            is_say = True
 
 
 if __name__ == "__main__":
